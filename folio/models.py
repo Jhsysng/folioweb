66656654bc4afa8794d7fdb12239d06c5c2462da
Jhsysng <screenshot161@gmail.com>
2023-03-29 13:21:08 +0900

    Portfolio init model

--- folio/models.py
+++ folio/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
+class Portfolio(models.Model):
+    title=models.CharField(max_length=50)
+    #tech_stack_tag
+    #urls
+
+    #goals
+    #motivation
+    #content
+    #images
+
+    #tag
+
+    created_at=models.DateTimeField(auto_now_add=True)
+    updated_at=models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f'{self.title}'
+
+
 # Create your models here.
