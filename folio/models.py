3eec12cb235127c1f46238dbc540ed346a62f690
Jhsysng <screenshot161@gmail.com>
2023-04-03 22:12:30 +0900

    core test model url link, motive model

--- folio/models.py
+++ folio/models.py
@@ -5,14 +5,13 @@ class Portfolio(models.Model):
     title=models.CharField(max_length=50)
     #tech_stack_tag
     #urls
-
+    code_url=models.CharField(max_length=70, null=True)
     #goals
-    #motivation
-    #content
-    content=models.TextField(null=True)
-    #image(sample_image)
-    image=models.ImageField(upload_to='folio/images/portfolio', blank=True)
 
+    #motivation
+    motive=models.TextField(null=True)
+    content=models.TextField(null=True)
+    image=models.ImageField(upload_to='folio/images/portfolio', blank=True)
 
     #tag
     #version
@@ -25,5 +24,3 @@ class Portfolio(models.Model):
 
     def get_url(self):
         return f'/folio/{self.pk}'
-
-# Create your models here.
