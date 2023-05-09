539b79b9dc51c93ca5c5b52ea7f4930a5cf436f3
Jhsysng <screenshot161@gmail.com>
2023-05-09 22:04:40 +0900

    distribution with postgresql

--- Portfolioblog/settings.py
+++ Portfolioblog/settings.py
@@ -82,12 +82,12 @@ WSGI_APPLICATION = 'Portfolioblog.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-        'OPTIONS': {
-            'timeout':30000,
-        },
-        'ATOMIC_REQUESTS': True,
+        'ENGINE': os.environ.get("SQL_ENGINE", 'django.db.backends.sqlite3'),
+        'NAME': os.environ.get("SQL_DATABASE", os.path.join(BASE_DIR, 'db.sqlite3')),
+        'USER': os.environ.get("SQL_USER", 'user'),
+        'PASSWORD': os.environ.get("SQL_PASSWORD", 'password'),
+        'HOST': os.environ.get("SQL_HOST", 'localhost'),
+        'PORT': os.environ.get("SQL_PORT", '5432'),
     }
 }
 
